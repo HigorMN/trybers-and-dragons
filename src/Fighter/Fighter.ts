43231783bfbce8dbdef8e348cf7ed4effe022633
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-02-16 19:30:20 -0300

    fix: importação

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,12 +1,14 @@
 import Energy from '../Energy';
 
-export default interface Fighter {
+interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
   energy?: Energy;
   attack(enemy: Fighter): void;
-  special(enemy: Fighter): void | undefined;
+  special?(enemy: Fighter): void;
   levelUp(): void;
   receiveDamage(attackPoints: number): number;
 }
+
+export default Fighter;
