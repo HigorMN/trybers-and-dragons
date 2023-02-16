1a773946966b2928ad0e52b2708a582b90e1ffe1
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-02-16 17:02:48 -0300

    feat: classe Orc

---
+++ src/Races/Orc.ts
@@ -0,0 +1,21 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private static count = 0;
+  private _maxLifePoints = 74;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Orc.count += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.count;
+  }
+}
+
+export default Orc;
