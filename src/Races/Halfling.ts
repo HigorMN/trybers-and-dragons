ac63f13bc098921fb5d7a5bd308700f7239d8236
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-02-16 17:02:37 -0300

    feat: classe Halfling

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,21 @@
+import Race from './Race';
+
+class Halfling extends Race {
+  private static count = 0;
+  private _maxLifePoints = 60;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Halfling.count += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Halfling.count;
+  }
+}
+
+export default Halfling;
