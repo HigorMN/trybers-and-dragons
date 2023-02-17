a0ce9ed5ddcea48cda984d9701a93cf85a8e134a
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-02-17 16:48:15 -0300

    feat: classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,23 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(public player1: Fighter, public player2: Fighter) {
+    super(player1);
+    this.player2 = player2;
+  }
+
+  fight(): number {
+    if (this.player1.lifePoints > -1 && this.player2.lifePoints > -1) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+
+    if (this.player1.lifePoints === -1) {
+      return -1;
+    } 
+    return 1;
+  }
+}
+
+export default PVP;
